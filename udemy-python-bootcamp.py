44fb4673d0a59ba05cddd3233fc428f6d1e8328a
Jacquelyn M <jacquelynkmccray@gmail.com>
2020-01-06 13:38:52 -0600

    Started udemy course

---
+++ udemy-python-bootcamp.py
@@ -0,0 +1,15 @@
+# Numbers
+# 2 to the power of 3:
+print(2 ** 3)
+
+# Escape a character
+print('hello\nworld')
+
+# Reverse a string
+astring = 'hello world'
+print(astring[::-1])
+
+# Print formatting with strings
+print('This is a string {}'.format('INSERTED'))
+print('The {1} {2} {0}'.format('fox','quick', 'brown'))
+print('The {q} {b} {f}'.format(f='fox',q='quick', b='brown'))
