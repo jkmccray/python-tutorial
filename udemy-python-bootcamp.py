f37820db980504eda0285251f0b106f3c3a7732d
Jacquelyn M <jacquelynkmccray@gmail.com>
2020-01-06 14:36:08 -0600

    completed udemy course through lists

--- udemy-python-bootcamp.py
+++ udemy-python-bootcamp.py
@@ -1,15 +1,71 @@
-# Numbers
+# ====== Numbers ======
 # 2 to the power of 3:
-print(2 ** 3)
+print(2 ** 3) # can also do roots this way
+# Floor division
+print (7//4)
+
+# ====== Strings ======
+# Strings are immutable in python
 
 # Escape a character
 print('hello\nworld')
 
 # Reverse a string
-astring = 'hello world'
-print(astring[::-1])
+a_string = 'hello world'
+print(a_string[::-1])
+
+# Repeat string with multiplication
+letters = 'zx'
+print(letters*10)
+
+# Built-in string methods
+new_string = 'Hello I am a string'
+print(new_string.upper())
+print(new_string.lower())
+print(new_string.split())
+print(new_string.split('a'))
+
 
 # Print formatting with strings
 print('This is a string {}'.format('INSERTED'))
 print('The {1} {2} {0}'.format('fox','quick', 'brown'))
 print('The {q} {b} {f}'.format(f='fox',q='quick', b='brown'))
+# Float formatting: {value:width.precision f}
+result = 100/777
+print('The result was {r:10.5f}'.format(r=result))
+
+# Formatted string literals
+name = 'John'
+print(f'Hello, his name is {name}')
+name = 'Sam'
+age = 3
+print(f'{name} is {age} years old')
+
+# ====== Lists ======
+my_list = [1,2,3]
+print(len(my_list))
+another_list = [4,5,6]
+new_list = my_list + another_list
+print(new_list)
+# reassign first element in the list
+new_list[0] = 'ONE ALL CAPS'
+print(new_list)
+# add to the list
+new_list.append('seven')
+print(new_list)
+# remove last item from the list
+new_list.pop()
+print(new_list)
+new_list.pop(3)
+print(new_list)
+
+# sort and reverse
+# both change the list in-place
+new_list = ['a', 'e', 'x', 'b', 'w']
+num_list = [4,1,7,2,8,3]
+print(type(num_list))
+new_list.sort()
+print(new_list)
+num_list.reverse()
+print(num_list)
+
